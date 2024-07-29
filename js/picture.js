17c99983b08b288aa9cbcb85368202fd77362436
Ramil Zaripov <zarram89@gmail.com>
2024-07-29 13:11:57 +0500

    Добавляет вывод модального окна с большим фото и комментами и его закрытием

--- js/picture.js
+++ js/picture.js
@@ -1,15 +1,22 @@
+import { showBigPicture } from './big-picture.js';
+
 const pictureTemplate = document
   .querySelector('#picture')
   .content.querySelector('.picture');
 const container = document.querySelector('.pictures');
 
-const createPicture = ({ comments, description, likes, url }) => {
+const createPicture = (data) => {
+  const { comments, description, likes, url } = data;
   const picture = pictureTemplate.cloneNode(true);
 
   picture.querySelector('.picture__img').src = url;
   picture.querySelector('.picture__img').alt = description;
   picture.querySelector('.picture__comments').textContent = comments.length;
   picture.querySelector('.picture__likes').textContent = likes;
+
+  picture.addEventListener('click', () => {
+    showBigPicture(data);
+  });
 
   return picture;
 };
