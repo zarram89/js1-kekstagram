04e2d17554d5fa1fd6639f9fe00c08f3bf93ebec
Ramil Zaripov <zarram89@gmail.com>
2024-09-23 12:14:00 +0500

    Добавляет окно формы и валидацию в рзаметку и скрипты

--- js/main.js
+++ js/main.js
@@ -1,10 +1,6 @@
-// import { getPictures } from './data.js';
-// import { renderPictures } from './picture.js';
-
-// renderPictures(getPictures());
-
 import { getPictures } from './data.js';
 import { renderPictures } from './picture.js';
+import './form.js';
 
 renderPictures(getPictures());
 
