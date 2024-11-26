eb80cd5bee8d774bfc110fef5a706c3cc73bfa2c
Ramil Zaripov <zarram89@gmail.com>
2024-11-26 12:34:25 +0500

    12.11. Добавляяет функции устранения дребезга и пропуска кадров

--- js/main.js
+++ js/main.js
@@ -1,7 +1,23 @@
-import { getPictures } from './data.js';
 import { renderPictures } from './picture.js';
-import './form.js';
+import { getData, sendData } from './api.js';
+import { showAlert } from './util.js';
+import { setOnFormSubmit, hideModal } from './form.js';
+import { showSuccessMessage, showErrorMessage } from './message.js';
 
 const pictureContainer = document.querySelector('.pictures');
 
-renderPictures(getPictures(), pictureContainer);
+const onSendDataSuccess = () => {
+  hideModal();
+  showSuccessMessage();
+};
+
+const onSendDataError = () => {
+  hideModal();
+  showErrorMessage();
+};
+
+setOnFormSubmit(async (data) => {
+  await sendData(onSendDataSuccess, onSendDataError, data);
+});
+
+getData(renderPictures, showAlert, pictureContainer);
--- js/util.js
+++ js/util.js
@@ -1,3 +1,5 @@
+const ALERT_SHOW_TIME = 5000;
+
 const getRandomPositiveInteger = (a, b) => {
   const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
   const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
@@ -7,7 +9,70 @@ const getRandomPositiveInteger = (a, b) => {
 
 const checkStringLength = (string, length) => string.length <= length;
 
-const getRandomArrayElement = (array) =>
-  array[getRandomPositiveInteger(0, array.length - 1)];
+const getRandomArrayElement = (array) => array[getRandomPositiveInteger(0, array.length - 1)];
 
-export { getRandomPositiveInteger, checkStringLength, getRandomArrayElement };
+const showAlert = (message) => {
+  const alert = document.createElement('div');
+  alert.style.position = 'absolute';
+  alert.style.zIndex = '100';
+  alert.style.left = '0';
+  alert.style.top = '0';
+  alert.style.right = '0';
+  alert.style.padding = '10px 3px';
+  alert.style.fontSize = '30px';
+  alert.style.textAlign = 'center';
+  alert.style.backgroundColor = 'red';
+  alert.textContent = message;
+  document.body.append(alert);
+
+  setTimeout(() => {
+    alert.remove();
+  }, ALERT_SHOW_TIME);
+};
+
+// Функция взята из интернета и доработана
+// Источник - https://www.freecodecamp.org/news/javascript-debounce-example
+
+function debounce (callback, timeoutDelay = 500) {
+  // Используем замыкания, чтобы id таймаута у нас навсегда приклеился
+  // к возвращаемой функции с setTimeout, тогда мы его сможем перезаписывать
+  let timeoutId;
+
+  return (...rest) => {
+    // Перед каждым новым вызовом удаляем предыдущий таймаут,
+    // чтобы они не накапливались
+    clearTimeout(timeoutId);
+
+    // Затем устанавливаем новый таймаут с вызовом колбэка на ту же задержку
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+
+    // Таким образом цикл «поставить таймаут - удалить таймаут» будет выполняться,
+    // пока действие совершается чаще, чем переданная задержка timeoutDelay
+  };
+}
+
+// Функция взята из интернета и доработана
+// Источник - https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_throttle
+
+function throttle (callback, delayBetweenFrames) {
+  // Используем замыкания, чтобы время "последнего кадра" навсегда приклеилось
+  // к возвращаемой функции с условием, тогда мы его сможем перезаписывать
+  let lastTime = 0;
+
+  return (...rest) => {
+    // Получаем текущую дату в миллисекундах,
+    // чтобы можно было в дальнейшем
+    // вычислять разницу между кадрами
+    const now = new Date();
+
+    // Если время между кадрами больше задержки,
+    // вызываем наш колбэк и перезаписываем lastTime
+    // временем "последнего кадра"
+    if (now - lastTime >= delayBetweenFrames) {
+      callback.apply(this, rest);
+      lastTime = now;
+    }
+  };
+}
+
+export { ALERT_SHOW_TIME, getRandomPositiveInteger, checkStringLength, getRandomArrayElement, showAlert, debounce, throttle, };
